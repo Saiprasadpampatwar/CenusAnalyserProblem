7a8e9f865c470fdeeb013ab2dff30fa32e57ac28
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2020-12-15 14:36:25 +0530

    [Saiprasad]Added: method for reading data from CSV using Common CSV

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -5,12 +5,16 @@ import com.opencsv.bean.CsvToBeanBuilder;
 import jar.builder.CSVBuilderException;
 import jar.builder.CSVBuilderFactory;
 import jar.builder.ICSVBuilder;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.List;
 import java.util.stream.StreamSupport;
 
 public class CensusAnalyser {
@@ -36,6 +40,19 @@ public class CensusAnalyser {
         }  catch (IOException | RuntimeException | CSVBuilderException e) {
             throw new CensusAnalyserException(e.getMessage(),
                     CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        }
+
+    }
+
+    public int loadCSVusingCommonCSV(String csvfilePath) throws CensusAnalyserException {
+        try(
+                Reader reader = Files.newBufferedReader(Paths.get(csvfilePath));
+                CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
+                ){
+            List<CSVRecord> csvRecords = csvParser.getRecords();
+            return csvRecords.size();
+        }catch (IOException e){
+            throw new CensusAnalyserException(e.getMessage(),CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
         }
 
     }
